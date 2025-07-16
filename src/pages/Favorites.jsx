717d94e2c977a12ac9db585c6a56c012bd7672e2
Ninjamsc <slimclick@gmail.com>
2025-07-16 21:25:05 +0000

    Jul 17, 2025, 12:25 AM

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,31 +1,35 @@
-import React from 'react';
-import Card from '../components/Card';
-import AppContext from '../context';
+import React from "react";
+import Card from "../components/Card";
+import AppContext from "../context";
 
-function Favorites({onAddToFavorite}) {
-  const {favorites} = React.useContext(AppContext)
+function Favorites({ onAddToFavorite }) {
+  const { favorites } = React.useContext(AppContext);
 
   return (
-    <div className='content p-40 '>
-      <div className='d-flex align-center justify-between mb-40'>
+    <div className="content p-40 ">
+      <div className="d-flex align-center justify-between mb-40">
         <h1>Мои закладки</h1>
       </div>
-      <div className='d-flex flex-wrap'>
-      {favorites
-            .map((item) => (
-              <Card
-                key={item.id}
-                id={item.id}
-                title={item.title}
-                price={item.price}
-                imageUrl={item.imageUrl}
-                favorited={true}
-                onFavorite={onAddToFavorite}
-              />
-            ))}
+      <div className="d-flex flex-wrap">
+        {favorites.map((item) => (
+          <Card
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            imageUrl={item.imageUrl}
+            favorited={true}
+            onFavorite={onAddToFavorite}
+          />
+        ))}
       </div>
     </div>
-  )
+  );
 }
+
+
+
+
+
 
 export default Favorites;
