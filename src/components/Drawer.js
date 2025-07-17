23b8c6ef7f020a8c3d0ab69c550e7c91a7c2a9df
Ninjamsc <slimclick@gmail.com>
2025-07-17 21:52:36 +0000

    Jul 18, 2025, 12:52 AM

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -19,7 +19,7 @@ function Drawer({ onClose, onRemove, items = [] }) {
         //   items: cartItems,
         // }
       );
-     await axios.post("http://localhost:3001/cart", []);
+     await axios.put("http://localhost:3001/cart", []);
       setOrderId(data.id);
       setIsOrderComplete(true);
       setCartItems([]);
