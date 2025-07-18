8c84d9bd1073c112a08ea327597bfc939fc6dbdc
Ninjamsc <slimclick@gmail.com>
2025-07-18 23:22:36 +0000

    Jul 19, 2025, 2:22 AM

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import AppContext from "../context";
 
-const Info = ({ title, image, decription }) => {
+const Info = ({ title, image, description }) => {
   const { setCartOpened } = React.useContext(AppContext);
 
-  console.log( "В Info переданы пропсы:",{title}, {image}, {decription});
-  
+  console.log( "В Info переданы пропсы:",{title}, {image}, {description});
+
   return (
     <div className="cartEmpty d-flex align-center justify-center flex-column flex">
       <img
@@ -16,7 +16,7 @@ const Info = ({ title, image, decription }) => {
         alt="text"
       />
       <h2>{title}</h2>
-      <p className="opacity-6">{decription}</p>
+      <p className="opacity-6">{description}</p>
       <button onClick={() => setCartOpened(false)} className="greenButton">
         <img src="/img/arrow.svg" alt="Arrow" />
         Вернуться назад
