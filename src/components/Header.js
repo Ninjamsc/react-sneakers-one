8123a447744c86e43e26ef044d60b4fd7f8cd648
Ninjamsc <slimclick@gmail.com>
2025-07-18 21:21:21 +0000

    Jul 19, 2025, 12:21 AM

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,29 @@
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 function Header(props) {
   return (
-
     <header className="d-flex space-between align-center p-40">
-      <Link to='/'>
+      <Link to="/">
         <div className="d-flex align-center">
-          <img width={40} height={40} src="/img/logo.png" alt='Logotype' />
-          <div className='headerInfo'>
-            <h3 className='text-uppercase'>React Sneakers </h3>
-            <p className='opacity-5'>Магазин лучших кроссовок</p>
+          <img width={40} height={40} src="/img/logo.png" alt="Logotype" />
+          <div className="headerInfo">
+            <h3 className="text-uppercase">React Sneakers </h3>
+            <p className="opacity-5">Магазин лучших кроссовок</p>
           </div>
         </div>
       </Link>
-      <ul className='d-flex'>
-        <li onClick={props.onClickCart} className='mr-25 cu-p'>
-          <img width={20} height={20} src="/img/Card.svg" alt='Корзина' />
+      <ul className="d-flex">
+        <li onClick={props.onClickCart} className="mr-25 cu-p">
+          <img width={20} height={20} src="/img/Card.svg" alt="Корзина" />
           <span>1205 руб.</span>
         </li>
-        <Link to='/favorites'>
+        <Link to="/favorites">
           <li className="mr-30 cu-p">
-            <img width={20} height={20} src="/img/Heart.svg" alt='Закладки' />
+            <img width={20} height={20} src="/img/Heart.svg" alt="Закладки" />
           </li>
         </Link>
         <li>
-          <img width={20} height={20} src="/img/User.svg" alt='Пользователь' />
+          <img width={20} height={20} src="/img/User.svg" alt="Пользователь" />
         </li>
       </ul>
     </header>
